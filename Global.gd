9c2561afb8b7e547e6fc3ac3b703ebc36e51d164
LukeSSJ <41447595+LukeSSJ@users.noreply.github.com>
2022-04-24 20:53:30 +0100

    Commit from PC

--- Global.gd
+++ Global.gd
@@ -5,6 +5,7 @@ var player
 var base
 
 var can_pause = false
+var pause_from_esc = false
 
 onready var Paused = $Paused
 
@@ -15,20 +16,30 @@ func _ready():
 func _unhandled_key_input(event):
 	if event.pressed and can_pause:
 		if event.scancode == KEY_ESCAPE:
-			pause(true)
+			toggle_pause()
 		elif event.scancode == KEY_F4:
 			OS.window_fullscreen = not OS.window_fullscreen
 
 func _notification(what):
 	if what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
 		pause()
+	elif what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
+		if not pause_from_esc:
+			unpause()
 
-func pause(toggle=false):
-	if not can_pause:
-		return
-		
-	var paused = true
-	if toggle:
-		paused = not get_tree().paused
-	get_tree().paused = paused
-	Paused.visible = paused
+func toggle_pause():
+	if get_tree().paused:
+		unpause()
+		pause_from_esc = false
+	else:
+		pause()
+		pause_from_esc = true
+
+func pause():
+	if can_pause:
+		get_tree().paused = true
+		Paused.visible = true
+
+func unpause():
+	get_tree().paused = false
+	Paused.visible = false
